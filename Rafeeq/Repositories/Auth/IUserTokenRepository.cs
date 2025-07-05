dd67fa62d29827a7d7396fbb3b145338d6bea2a0
HamdyAboyousef <hamdysalahmohamed.iti@gmail.com>
2025-07-05 03:22:53 +0300

    add AuhtCotntroller 3.22am

--- Rafeeq/Repositories/Auth/IUserTokenRepository.cs
+++ Rafeeq/Repositories/Auth/IUserTokenRepository.cs
@@ -3,12 +3,10 @@ using Rafeeq.Repositories.RepositoryBase;
 
 namespace Rafeeq.Repositories.Auth
 {
-    public interface IUserTokenRepository: IRepositoryBase<UserToken>
+    public interface IUserTokenRepository : IRepositoryBase<UserToken>
     {
         Task<UserToken?> GetTokenByValueAndTypeAsync(string tokenValue, string tokenType);
         Task<List<UserToken>> GetActiveTokensForUserAsync(int userId, string tokenType);
-        Task<List<UserToken>> GetTokensForUserInTimeRange(int userId, string tokenType, DateTime sinceDateTime); // Add this method
-
 
     }
 }
