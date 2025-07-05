c911fca1e73d9c92ef4bf3d9a2ae0310eed7f359
reda abd elglel <redaelnggar222@gmail.com>
2025-07-05 14:06:12 +0300

    modifing the logic of google meet

---
+++ Rafeeq/DTOs/Bookings/UpdateMeetingLinkDto.cs
@@ -0,0 +1,12 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Rafeeq.DTOs.Bookings
+{
+    public class UpdateMeetingLinkDto
+    {
+        [Required]
+        [Url(ErrorMessage = "Please provide a valid meeting link")]
+        public string MeetingLink { get; set; }
+    }
+}
