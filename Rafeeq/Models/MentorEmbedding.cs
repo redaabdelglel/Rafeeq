306b32f641c285ae7b8a57945c6c9fb7054579f6
reda abd elglel <redaelnggar222@gmail.com>
2025-07-07 05:50:47 +0300

    after adding the new tables of ai to the database i modified the context and add the models

---
+++ Rafeeq/Models/MentorEmbedding.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rafeeq.Models
+{
+    public class MentorEmbedding
+    {
+        [Key]
+        public int EmbeddingId { get; set; }
+
+        public int? UserId { get; set; }
+
+        public byte[]? BioEmbedding { get; set; } // Mentor bio + skills as vector
+
+        [Column(TypeName = "datetime")]
+        public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("UserId")]
+        [InverseProperty("MentorEmbedding")]
+        public virtual User? User { get; set; }
+    }
+}
