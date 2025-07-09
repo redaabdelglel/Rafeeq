c2021327046697bf3f7865033f72e4215a7efe4c
reda abd elglel <redaelnggar222@gmail.com>
2025-07-09 16:35:52 +0300

    adding models for Forum categories,posts,comments,upvotes

---
+++ Rafeeq/Models/ForumPostUpvote.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rafeeq.Models
+{
+    public class ForumPostUpvote
+    {
+        [Key]
+        public int UpvoteId { get; set; }
+
+        [Required]
+        public int PostId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation
+        [ForeignKey("PostId")]
+        public ForumPost Post { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
